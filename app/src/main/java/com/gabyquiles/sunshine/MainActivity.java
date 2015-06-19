986067d9242e4451da4e069e67657b50971cb9b9
Gabriel Quiles-Perez <info@gabyquiles.com>
2015-06-19 17:33:25 -0400

    Add menu option to show prefered location on map. Some code cleaning

--- app/src/main/java/com/gabyquiles/sunshine/MainActivity.java
+++ app/src/main/java/com/gabyquiles/sunshine/MainActivity.java
@@ -2,11 +2,16 @@ package com.gabyquiles.sunshine;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     public final static String FORECAST_DATA = "com.gabyquiles.sunshine.FORECAST_DATA";
 
     @Override
@@ -35,8 +40,30 @@ public class MainActivity extends Activity {
             Intent intent = new Intent(this, SettingsActivity.class);
             this.startActivity(intent);
             return true;
+        } else if(id == R.id.action_location_map) {
+            showPreferedLocationOnMap();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showPreferedLocationOnMap() {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = preferences.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+                .appendEncodedPath("0,0")
+                .appendQueryParameter("q", location);
+        showMap(builder.build());
+    }
+
+    private void showMap(Uri geoLocation) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.w(LOG_TAG, "No activity found to receive the intent.");
+        }
     }
 }
